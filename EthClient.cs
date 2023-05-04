61f6d1c1a252d216ea7e7ec945103db1e781a9aa
Joey <joey.xf@gmail.com>
2023-05-04 19:41:53 +0800

    craw and produce

--- EthClient.cs
+++ EthClient.cs
@@ -13,6 +13,7 @@ using Nethereum.BlockchainProcessing.Processor;
 using Nethereum.BlockchainStore.EFCore.Sqlite;
 using Nethereum.BlockchainStore.EFCore.Repositories;
 using Nethereum.BlockchainStore.EFCore;
+using Confluent.Kafka;
 
 public class EthClient
 {
@@ -23,16 +24,16 @@ public class EthClient
         this.web3 = web3;
     }
 
-    public async Task CrawlBlocks()
+    public async Task CrawlBlocks(IProducer<string, string> producer)
     {
-        var blocks = new List<BlockWithTransactions>();
-        var transactions = new List<TransactionReceiptVO>();
-        var contractCreations = new List<ContractCreationVO>();
-        var filterLogs = new List<FilterLogVO>();
+        // var blocks = new List<BlockWithTransactions>();
+        // var transactions = new List<TransactionReceiptVO>();
+        // var contractCreations = new List<ContractCreationVO>();
+        // var filterLogs = new List<FilterLogVO>();
 
         var context = new SqliteBlockchainDbContextFactory($"Data Source=nft_market.db");
         var repoFactory = new BlockchainStoreRepositoryFactory(context);
-        
+
         // var processor = web3.Processing.Blocks.CreateBlockProcessor(steps =>
         // {
         //     steps.BlockStep.AddSynchronousProcessorHandler(b => blocks.Add(b));
@@ -41,7 +42,12 @@ public class EthClient
         //     steps.FilterLogStep.AddSynchronousProcessorHandler(l => filterLogs.Add(l));
         // });
 
-        var processor = this.web3.Processing.Blocks.CreateBlockStorageProcessor(repoFactory);
+        var processor = this.web3.Processing.Blocks.CreateBlockStorageProcessor(repoFactory, configureSteps: steps =>
+        {
+            steps.BlockStep.AddSynchronousProcessorHandler(b => producer.Produce("raw_blocks", new Message<string, string> { Key = b.Number.Value.ToString(), Value = JsonSerializer.Serialize(b) }));
+            steps.TransactionReceiptStep.AddSynchronousProcessorHandler(tx => producer.Produce("raw_txs", new Message<string, string> { Key = tx.TransactionHash, Value = JsonSerializer.Serialize(tx) }));
+            steps.FilterLogStep.AddSynchronousProcessorHandler(l => producer.Produce("raw_logs", new Message<string, string> { Key = l.Log.TransactionHash, Value = JsonSerializer.Serialize(l) }));
+        });
 
         //if we need to stop the processor mid execution - call cancel on the token
         var cancellationToken = new CancellationToken();
@@ -53,21 +59,27 @@ public class EthClient
         //   startAtBlockNumberIfNotProcessed: new BigInteger(3269520));
         await processor.ExecuteAsync(cancellationToken: cancellationToken);
 
-        Console.WriteLine($"Blocks Found: {blocks.Count}");
-        Console.WriteLine($"Transactions Found: {transactions.Count}");
-        Console.WriteLine($"Contract Creations Found: {contractCreations.Count}");
+        // Console.WriteLine($"Blocks Found: {blocks.Count}");
+        // Console.WriteLine($"Transactions Found: {transactions.Count}");
+        // Console.WriteLine($"Contract Creations Found: {contractCreations.Count}");
 
-        foreach(var l in filterLogs) {
-            if (l.IsLogForEvent<TransferEvent1>()) {
-                var logEvent = l.Log.DecodeEvent<TransferEvent1>();
-                Console.WriteLine($"event1: from: {logEvent.Event.From}, to: {logEvent.Event.To}, value: {logEvent.Event.Value}");
-            } else if (l.IsLogForEvent<TransferEvent2>()) {
-                var logEvent = l.Log.DecodeEvent<TransferEvent2>();
-                Console.WriteLine($"event2: from: {logEvent.Event.From}, to: {logEvent.Event.To}, value: {logEvent.Event.Value}");
-            } else {
-                Console.WriteLine("other event");
-            }
-        }
+        // foreach (var l in filterLogs)
+        // {
+        //     if (l.IsLogForEvent<TransferEvent1>())
+        //     {
+        //         var logEvent = l.Log.DecodeEvent<TransferEvent1>();
+        //         Console.WriteLine($"event1: from: {logEvent.Event.From}, to: {logEvent.Event.To}, value: {logEvent.Event.Value}");
+        //     }
+        //     else if (l.IsLogForEvent<TransferEvent2>())
+        //     {
+        //         var logEvent = l.Log.DecodeEvent<TransferEvent2>();
+        //         Console.WriteLine($"event2: from: {logEvent.Event.From}, to: {logEvent.Event.To}, value: {logEvent.Event.Value}");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("other event");
+        //     }
+        // }
 
     }
 }
